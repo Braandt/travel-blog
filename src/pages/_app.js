c21d1b93fb197ea461458bd1f6dbfb70268f293e
Braandt <leoobrandt@hotmail.com>
2023-07-03 15:08:17 -0300

    Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import Footer from '@/components/sections/Footer'
 import Nav from '@/components/sections/Nav'
 import '@/styles/globals.css'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
 	return (
